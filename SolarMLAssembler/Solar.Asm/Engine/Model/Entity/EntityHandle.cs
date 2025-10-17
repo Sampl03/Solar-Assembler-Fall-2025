42d9c8ab829f92861a98bf101b874304c2fa6c7f
Sampl03 <spier033@uottawa.ca>
2025-10-17 19:17:34 -0400

    Create EntityHandle

---
+++ SolarMLAssembler/Solar.Asm/Engine/Model/Entity/EntityHandle.cs
@@ -0,0 +1,6 @@
+﻿namespace Solar.Asm.Engine.Model.Entity
+{
+    public class EntityHandle<EntityType> where EntityType : ModelEntity
+    {
+    }
+}
--- SolarMLAssembler/Solar.Asm/Engine/Model/Entity/ModelEntity.cs
+++ SolarMLAssembler/Solar.Asm/Engine/Model/Entity/ModelEntity.cs
@@ -1,5 +1,6 @@
 ﻿namespace Solar.Asm.Engine.Model.Entity
 {
+    // Represents an entity in the model which can be accessed via an opaque handle
     public abstract class ModelEntity
     {
         public uint ID { get; protected set; }
