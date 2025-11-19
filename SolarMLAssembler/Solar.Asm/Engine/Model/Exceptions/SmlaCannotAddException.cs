7f3f44fbc533bc54e35d961f35fdf705bca6af3d
Sampl03 <spier033@uottawa.ca>
2025-11-18 20:08:06 -0500

    Added new exceptions types for the Engine

---
+++ SolarMLAssembler/Solar.Asm/Engine/Model/Exceptions/SmlaCannotAddException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Solar.Asm.Engine.Model.Exceptions
+{
+    public class SmlaCannotAddException : SmlaException
+    {
+        public SmlaCannotAddException() { }
+        public SmlaCannotAddException(string message) : base(message) { }
+        public SmlaCannotAddException(string message, Exception inner) : base(message, inner) { }
+    }
+}
---
+++ SolarMLAssembler/Solar.Asm/Engine/Model/Exceptions/SmlaException.cs
@@ -0,0 +1,9 @@
+﻿namespace Solar.Asm.Engine.Model.Exceptions
+{
+    public class SmlaException : Exception
+    {
+        public SmlaException() { }
+        public SmlaException(string message) : base(message) { }
+        public SmlaException(string message, Exception inner) : base(message, inner) { }
+    }
+}
