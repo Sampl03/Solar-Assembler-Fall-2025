7a926f4f0848800938d1c718d17bdc4173a544fb
Sampl03 <spier033@uottawa.ca>
2025-12-05 01:10:03 -0500

    Created StructType for later MetaEntity implementations

---
+++ SolarMLAssembler/Solar.Asm/Engine/Model/Meta/StructType.cs
@@ -0,0 +1,11 @@
+﻿using Solar.EntitySystem;
+
+namespace Solar.Asm.Engine.Model.Meta
+{
+    public class StructType : MetaEntity
+    {
+        public StructType() : base()
+        {
+        }
+    }
+}
