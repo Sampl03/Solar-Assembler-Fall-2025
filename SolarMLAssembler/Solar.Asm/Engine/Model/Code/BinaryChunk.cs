652412309800a3a48bfcf338d4941859cf65b958
Sampl03 <spier033@uottawa.ca>
2025-12-07 13:39:13 -0500

    Create BinaryChunk
    Represents a chunk of arbitrary binary data

---
+++ SolarMLAssembler/Solar.Asm/Engine/Model/Code/BinaryChunk.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Solar.Asm.Engine.Model.Code
+{
+    public sealed class BinaryChunk(byte[] data, BinaryPatch[] patches) : Chunk
+    {
+        public override IReadOnlyList<byte> EmitBytes()
+        {
+            GuardValidity();
+
+            if (patches.Length > 0)
+                data = OwningProgram.SharedMeta.AssemblyDialect.PatchBytes(data, patches);
+
+            return (byte[])data.Clone();
+        }
+
+        public override BinaryPatch[] EmitPatches() {
+            GuardValidity();
+
+            return (BinaryPatch[])patches.Clone();
+        }
+    }
+}
